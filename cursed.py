94f61ec6c671d2bc330fc4adffbce6885b8eedbd
Rose Gold <76526946+RoseGoldIsntGay@users.noreply.github.com>
2022-04-21 19:01:14 +0300

    i love python

---
+++ cursed.py
@@ -0,0 +1,53 @@
+# Multiple expressions in one lambda
+(
+    lambda _=None: (
+        print("I"),
+        print("love"),
+        print("Python!")
+    )
+)()
+
+# Assign variables in lambda
+(
+    lambda _=None: (
+        # globals is a dictionary that holds all global variables in script
+        # .update replaces '='
+        # {} anonymous dictionary where key is the variable's name and value is the variable's value
+        globals().update({"num": 1}),
+        print(num),
+    )
+)()
+
+# If statement in lambda
+(lambda _=None: (
+    globals().update({"number": 0}),
+    number == 0 and print("number is 0"),
+    globals().update({"number": 14}),
+    number == 0 and print("number is 0") or print("number is not 0")
+))()
+
+# For loop in lambda
+(
+    lambda _=None: (
+        # for i in range(1):
+        #    print(i)
+        [(lambda x: (
+            print(x)
+        ))(i) for i in range(10)]
+    )
+)()
+
+# While loop in lambda (recursion)
+(lambda _=None: (
+    globals().update({"count": 0}),
+    globals().update({"func": (lambda _=None: (
+        # count += 1
+        globals().update({"count": globals().get("count") + 1}),
+        print(count),
+
+        # if count < 10: 
+        #    func()
+        count < 10 and func()
+    ))}),
+    func()
+))()
